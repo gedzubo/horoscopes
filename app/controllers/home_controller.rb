1c361537270a24f7b9df579ce22e3620730be35e
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-01-29 16:01:55 +0000

    Adding individual sign routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,52 @@ class HomeController < ApplicationController
   def index
     @signs = Sign.all
   end
+
+  def aries
+    render "sign"
+  end
+
+  def taurus
+    render "sign"
+  end
+
+  def gemini
+    render "sign"
+  end
+
+  def cancer
+    render "sign"
+  end
+
+  def leo
+    render "sign"
+  end
+
+  def virgo
+    render "sign"
+  end
+
+  def libra
+    render "sign"
+  end
+
+  def scorpio
+    render "sign"
+  end
+
+  def sagittarius
+    render "sign"
+  end
+
+  def capricorn
+    render "sign"
+  end
+
+  def aquarius
+    render "sign"
+  end
+
+  def pisces
+    render "sign"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,20 @@ Rails.application.routes.draw do
   resources :signs, only: [ :index, :show, :edit, :update ]
   resource :session
   resources :passwords, param: :token
+
+  get "aries" => "home#aries"
+  get "taurus" => "home#taurus"
+  get "gemini" => "home#gemini"
+  get "cancer" => "home#cancer"
+  get "leo" => "home#leo"
+  get "virgo" => "home#virgo"
+  get "libra" => "home#libra"
+  get "scorpio" => "home#scorpio"
+  get "sagittarius" => "home#sagittarius"
+  get "capricorn" => "home#capricorn"
+  get "aquarius" => "home#aquarius"
+  get "pisces" => "home#pisces"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
